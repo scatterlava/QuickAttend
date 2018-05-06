9972b246f41451a9a2d1c7d4983e8ddeeb0dd0f1
Nishant Saxena <25915502+scatterlava@users.noreply.github.com>
2018-05-05 19:28:53 -0700

    xml done, working on getting information from jsonobject "currentuser" in java file

--- app/src/main/java/com/example/nishant/quickattend/AccountFragment.java
+++ app/src/main/java/com/example/nishant/quickattend/AccountFragment.java
@@ -1,46 +1,47 @@
 package com.example.nishant.quickattend;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.nishant.quickattend.API.APIClient;
+import com.example.nishant.quickattend.API.SectionService;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AccountFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AccountFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ClassesFragment.OnFragmentInteractionListener mListener;
+    /*private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";*/
+    private static EditText Cfirstname, Clastname, CSID, Cmail;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private String fname = "", lname = "", studentid = "", email = "";
+
+    private SectionService sectionService;
 
     public AccountFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
+   Button logout;
+
     // TODO: Rename and change types and number of parameters
     public static AccountFragment newInstance() {
         AccountFragment fragment = new AccountFragment();
@@ -50,17 +51,70 @@ public class AccountFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        sectionService = APIClient.getClient().create(SectionService.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        //EditText linked
+        Cfirstname = v.findViewById(R.id.text1);
+        Clastname = v.findViewById(R.id.text2);
+        CSID = v.findViewById(R.id.text3);
+        Cmail = v.findViewById(R.id.text4);
+
+        //get data for current user
+
+        Call<JsonElement> call = sectionService.getCurrent();
+        call.enqueue(new Callback<JsonElement>() {
+                         @Override
+                         public void onResponse(Call<JsonElement> call, Response<JsonElement> response) {
+                             if (response.isSuccessful()) {
+                                 JsonObject jResponse = response.body().getAsJsonObject();
+
+                                 //TODO Get shared preference then loop with session id
+                                 SharedPreferences sharedPref = getActivity().getSharedPreferences
+                                         (getString(R.string.shared_preference), Context.MODE_PRIVATE);
+
+                                 try {
+                                     JSONObject currentUser = new JSONObject(sharedPref.getString("currentUser", null));
+                                     if (currentUser != null) {
+                                         fname = currentUser.getString("firstname");
+                                         lname = currentUser.getString("lastname");
+                                         studentid = currentUser.getString("_id");
+                                         email = currentUser.getString("email");
+                                     }
+                                 } catch (JSONException e) {
+                                     e.printStackTrace();
+                                 }
+                             }
+                         }
+            @Override
+            public void onFailure(Call<JsonElement> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
+        //set data to editText fields in account
+        Cfirstname.setText(fname);
+        Clastname.setText(lname);
+        CSID.setText(studentid);
+        Cmail.setText(email);
+
+        logout = v.findViewById(R.id.LogoutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //onclicking the logout button:
+                //it should "log out" current user and go back to login page
+
+            }
+        });
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -74,7 +128,7 @@ public class AccountFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+          //  mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -87,18 +141,9 @@ public class AccountFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+     //   void onFragmentInteraction(Uri uri);
     }
 }
